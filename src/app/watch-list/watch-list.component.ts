237f089aea28d69d425f21d425feb747fe7882c6
poswald338 <poswald338@gmail.com>
2021-07-12 12:04:33 -0600

    some cleanup
    added a carousel for items displayed in my watch-list

--- src/app/watch-list/watch-list.component.ts
+++ src/app/watch-list/watch-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { Movie } from '../movies/movie.model';
 import { WatchListService } from './watchList.service';
 
@@ -9,9 +10,12 @@ import { WatchListService } from './watchList.service';
 })
 export class WatchListComponent implements OnInit {
   movies: Movie[];
+  movieWatched = false;
+  rating = 0;
 
 
   constructor(private watchListService: WatchListService) { }
+  @ViewChild('ngcarousel', { static: true }) ngCarousel: NgbCarousel;
 
   ngOnInit(): void {
     this.movies = this.watchListService.getMovies()
@@ -20,4 +24,26 @@ export class WatchListComponent implements OnInit {
     })
   }
 
+  onMovieWatched(index) {
+    this.movieWatched = true;
+    this.watchListService.addWatchedMovie(index);
+
+  }
+
+  onMovieReviewed(index) {
+    this.movieWatched = false;
+    this.watchListService.deleteMovie(index);
+  }
+
+  onRemove(index) {
+    this.watchListService.deleteMovie(index)
+  }
+
+  getToPrev() {
+    this.ngCarousel.prev()
+  }
+
+  goToNext () {
+    this.ngCarousel.next()
+  }
 }
